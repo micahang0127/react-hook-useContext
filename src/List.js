6f86302144e7f6906243ec7c7f4e118df3540ea6
sanga.park <sanga.park@epikar.com>
2022-11-24 10:12:11 +0900

    useContext

---
+++ src/List.js
@@ -0,0 +1,15 @@
+import React, { useContext } from "react";
+import Item from "./Item";
+import { TodoContext } from "./App";
+
+const List = () => {
+  const { todos, loading, changeTodoStatus } = useContext(TodoContext);
+  let todoList = <div>loading...</div>;
+  if (!loading)
+    todoList = todos.map((todo) => (
+      <Item key={todo.id} todo={todo} changeTodoStatus={changeTodoStatus} />
+    ));
+  return <ul>{todoList}</ul>;
+};
+
+export default List;
