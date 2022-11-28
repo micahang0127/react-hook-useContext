526ae55f6c85ba6e2ef8610fe6e208be9bee1f8e
sanga.park <psa5013@hanmail.net>
2022-11-28 10:31:22 +0900

    customHooks

---
+++ src/useFetch.js
@@ -0,0 +1,21 @@
+import {useState, useEffect} from 'react';
+
+const useFetch = (callback, url) => {
+    const [loading, setLoading] = useState(false);
+    const fetchInitialData = async () => {
+      setLoading(true);
+      const response = await fetch(url);
+      const initialData = await response.json();
+      console.log('서버로 부터 데이터를 받아오는 fetchInitialData 호출됨', initialData);
+      callback(initialData);
+      setLoading(false);    
+    }
+    useEffect(() => {
+      console.log('useFetch의 한번만 호출되는 useEffect');
+      fetchInitialData();
+    }, []);
+    
+    return loading;
+  }
+
+export default useFetch;
